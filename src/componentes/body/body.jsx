cefebc2cbe0b9daddeca6e51b83d35134a9d4171
Kristich Juan Ignacio <j.kristich@etec.local>
2023-08-11 12:42:24 -0300

    copia 3

---
+++ src/componentes/body/body.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import iphone1 from "../../multimedia/iphone14logo.png";
+import gadget from "../../multimedia/auricular.png";
+import './body.css';
+export default function Body() {
+  return <>
+            {/* <div class="card" style="width: 80%;">
+                    <img src="/Multimedia/starhub-store-iphone-14-pro-max-gold-front.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="celulares.html" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 80%;">
+                    <img src="Multimedia/auricular.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="gagets prueba.html" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                --> */}
+
+            <main>
+                <div class="Producto">
+                    <a href="celulares.html"> <img src={iphone1} alt="" class="producto-logo"/> </a>
+                </div>
+                                    
+                <div class="Producto">
+                    <a href="gagets prueba.html"><img src={gadget} alt="" class="producto-logo"/></a>
+                </div>
+
+            </main>
+    </>
+}
--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -1,17 +1,58 @@
 import './header.css';
-import carritopng from '';
-import logo_empresa_KRC from "../../multimedia/KR store logo.png";
-import gift from "../../multimedia/";
+import acount_circle from '../../multimedia/account_circle.png'
+import cart from "../../multimedia/shopping_cart.png"
+import logo from "../../multimedia/KR-store-logo.png"
 export default function Header(){
-    return
+    return(
 
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>R.K Store</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="./CSS/main.css"/>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-
-
+        // <header>
+        //     <nav class="navbar navbar-expand-lg bg-body-tertiary" >
+        //         <div class="container-fluid">
+        //             <a class="navbar-brand" href="Index.html"><img src="./Multimedia/KR store logo.png" alt="" style="max-width: 15%;" /></a>
+        //             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //                 <span class="navbar-toggler-icon"></span>
+        //             </button>
+        //             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        //                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        //                     <li class="nav-item">
+        //                         <a class="nav-link active" aria-current="page" href="#">Home</a>
+        //                     </li>
+        //                     <li class="nav-item">
+        //                         <a class="nav-link" href="#">Link</a>
+        //                     </li>
+        //                     <li class="nav-item dropdown">
+        //                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        //                         Dropdown
+        //                         </a>
+        //                         <ul class="dropdown-menu">
+        //                         <li><a class="dropdown-item" href="#">Action</a></li>
+        //                         <li><a class="dropdown-item" href="#">Another action</a></li>
+        //                         <li><hr class="dropdown-divider" /></li>
+        //                         <li><a class="dropdown-item" href="#">Something else here</a></li>
+        //                         </ul>
+        //                     </li>
+        //                     <li class="nav-item">
+        //                         <a class="nav-link disabled">Disabled</a>
+        //                     </li>
+        //                 </ul>
+        //                 <form class="d-flex" role="search">
+        //                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        //                     <button class="btn btn-outline-success" type="submit">Search</button>
+        //                 </form>
+        //             </div>
+        //         </div>
+        //     </nav>
+        <header>
+            <nav>
+                <h1>
+                    <img class="logo_kr" src={logo} alt="APP LOGO"/>
+                </h1>
+                <ul class="nav_list">
+                    <li class="nav_link"><a href="#"><img src={""} alt=""/>as</a></li> 
+                    <li class="nav_link"><a href="#"><img src={acount_circle} alt=""/></a></li>
+                    <li class="nav_link"><a href="#"><img src={cart} alt=""/></a></li>
+                </ul>
+            </nav> 
+        </header>
+    )
 }
