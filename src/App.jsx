f7d277b4c085dd042b463e91a98590e32a30ccae
JuanIgnacioKristich <j.kristich@alumno.etec.um.edu.ar>
2023-08-15 23:47:51 -0300

    main-footer-cat

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/header/header';
 import Body from './componentes/body/body';
-
+import Footer from "./componentes/footer/footer"
+import Cat_Celulares from './componentes/celulares/cat_cel';
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
-      
+      <Footer/>
     </div>
     
   );
---
+++ src/componentes/celulares/cat_cel.jsx
@@ -0,0 +1,34 @@
+
+import './cat_cel.css';
+import iphone_14_pro_max from "../../multimedia/iphone-14-pro-max-.png"
+import iphone_14_plus from "../../multimedia/iphone-14-plus.png"
+import iphone_13_pro_max from "../../multimedia/iphone-13-pro-max.png"
+import iphone_13 from "../../multimedia/iphone-13-green.png"
+import iphone_13_select from "../../multimedia/iphone-13-green-select.png"
+export default function Cat_Celulares(){
+    return<>
+        <main>
+            <div class="Producto">
+                <img src={iphone_14_pro_max} alt="" class="producto-cat-cel"/><button ><a href="#"></a> Iphone 14 Pro Max</button>
+            </div>
+
+            <div class="Producto">
+                <img src={iphone_14_plus} class="producto-cat-cel"/><button ><a href="#"></a> Iphone 14</button>
+            </div>
+            
+            <div class="Producto">
+                <img src={iphone_13_pro_max} alt="" class="producto-cat-cel"/><button ><a href="#"></a> Iphone 13 pro max</button>
+            </div>
+            
+            <div class="Producto">
+                <img src={iphone_13} alt="" class="producto-cat-cel"/><button ><a href="#"></a> Celulares</button>
+            </div>
+            
+            <div class="Producto">
+                <img src={iphone_13_select} alt="" class="producto-cat-cel"/><button ><a href="#"></a> Celulares</button>
+            </div>
+            
+        </main>
+    
+    </>
+}
